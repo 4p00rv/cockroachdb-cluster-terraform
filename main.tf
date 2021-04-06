88fe2023fb85436b733f84db8cb3bd82144b3fea
Apoorv Joshi <apoorv@binary.com>
2021-04-06 19:33:17 +0800

    add rule for ssh access

--- main.tf
+++ main.tf
@@ -21,6 +21,15 @@ resource "aws_security_group_rule" "dashboard" {
   security_group_id = aws_security_group.default.id
 }
 
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = [var.personal_cidr]
+  security_group_id = aws_security_group.default.id
+}
+
 resource "aws_security_group_rule" "db_port" {
   type              = "ingress"
   from_port         = var.db_port
@@ -31,11 +40,12 @@ resource "aws_security_group_rule" "db_port" {
 }
 
 resource "aws_instance" "cockroachdb" {
-  ami             = var.ami_id
-  instance_type   = "t2.micro"
-  key_name        = var.ssh_key_name
-  subnet_id       = module.vpc.subnet_id
-  security_groups = [aws_security_group.default.id]
+  ami                          = var.ami_id
+  associate_public_ip_address  = true
+  instance_type                = "t2.micro"
+  key_name                     = var.ssh_key_name
+  subnet_id                    = module.vpc.subnet_id
+  security_groups              = [aws_security_group.default.id]
 
   volume_tags = { Name = "test" }
 
